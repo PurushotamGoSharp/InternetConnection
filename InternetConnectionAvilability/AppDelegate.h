9be54f33bb7fa66c5f1778299a641056f98e6f7f
Purushottam <purushottamk@vmokshagroup.com>
2017-02-13 09:51:19 +0530

    Initial Commit

---
+++ InternetConnectionAvilability/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InternetConnectionAvilability
+//
+//  Created by vmoksha mobility on 2/13/17.
+//  Copyright © 2017 vmoksha mobility. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
